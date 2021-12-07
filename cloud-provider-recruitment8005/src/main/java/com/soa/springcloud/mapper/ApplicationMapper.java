3c6f9ec4b3a9358c1b41cf810a7119ddd1ac9f3f
Aiysosis <aiysosis@qq.com>
2021-12-07 19:20:25 +0800

    “完成了招聘模块的大部分功能”

---
+++ cloud-provider-recruitment8005/src/main/java/com/soa/springcloud/mapper/ApplicationMapper.java
@@ -0,0 +1,7 @@
+package com.soa.springcloud.mapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.soa.springcloud.entities.Application;
+
+public interface ApplicationMapper extends BaseMapper<Application> {
+
+}
