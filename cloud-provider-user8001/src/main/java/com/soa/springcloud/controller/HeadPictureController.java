189bebe6b082ed903ef289e7deba40fb0073b6f2
蒋汶霖 <465955336@qq.com>
2021-12-07 20:06:22 +0800

    个人信息模块基本方法结束

--- cloud-provider-user8001/src/main/java/com/soa/springcloud/controller/HeadPictureController.java
+++ cloud-provider-user8001/src/main/java/com/soa/springcloud/controller/HeadPictureController.java
@@ -1,8 +1,13 @@
 package com.soa.springcloud.controller;
 
+import com.soa.springcloud.entities.CommonResult;
 import com.soa.springcloud.service.HeadPictureService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @Slf4j
@@ -11,5 +16,20 @@ public class HeadPictureController {
 
     public HeadPictureController(HeadPictureService headPictureService) {
         this.headPictureService = headPictureService;
+    }
+
+    @PostMapping("/userinfo/pic")
+    public CommonResult addHeadPicture(@RequestParam("unifiedId") Integer unified_id,
+                                       @RequestPart("file") MultipartFile file) {
+        //预处理传入参数
+        if(unified_id ==null){
+            return CommonResult.failure("失败，unifiedId空");
+        }
+        //开始添加头像
+        int i=0;
+        if(i >0) {
+            return CommonResult.success("添加简历成功",null);
+        }
+        return CommonResult.failure("添加简历失败");
     }
 }
