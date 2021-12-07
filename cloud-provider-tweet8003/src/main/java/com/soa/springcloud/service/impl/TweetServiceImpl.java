0d54398cd61a4c78ccd734c3a0638f4486a3d381
Mizu <2579587457@qq.com>
2021-12-07 19:14:38 +0800

    部分Tweet

--- cloud-provider-tweet8003/src/main/java/com/soa/springcloud/service/impl/TweetServiceImpl.java
+++ cloud-provider-tweet8003/src/main/java/com/soa/springcloud/service/impl/TweetServiceImpl.java
@@ -1,5 +1,8 @@
 package com.soa.springcloud.service.impl;
 
+import cn.hutool.json.JSONArray;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.soa.springcloud.entities.Comment;
 import com.soa.springcloud.entities.Tweet;
@@ -20,7 +23,7 @@ public class TweetServiceImpl implements TweetService{
     @Resource
     private CommentMapper commentMapper;
 
-
+    @Override
     public int addLikeNum(Integer tweetId){
 
         Tweet tweet = tweetMapper.selectById(tweetId);
@@ -32,7 +35,7 @@ public class TweetServiceImpl implements TweetService{
         tweetMapper.update(tweet,queryWrapper);
         return tweetMapper.selectById(tweetId).getPraiseNum();
     }
-
+    @Override
     public int subtractLikeNum(Integer tweetId){
         Tweet tweet = tweetMapper.selectById(tweetId);
         QueryWrapper<Tweet> queryWrapper = new QueryWrapper<>();
@@ -42,6 +45,26 @@ public class TweetServiceImpl implements TweetService{
         tweet.setPraiseNum(tweet.getPraiseNum()-1);
         tweetMapper.update(tweet,queryWrapper);
         return tweetMapper.selectById(tweetId).getPraiseNum();
+    }
+    @Override
+    public int deleteTweet(Integer tweetId){
+        QueryWrapper<Tweet> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("tweet_id",tweetId);
+        return tweetMapper.delete(queryWrapper);
+    }
+
+    public int getSelfTweetList(Integer intervieweeId, Integer momentId){
+        QueryWrapper<Tweet> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("unified_id",intervieweeId).gt("moment_id",momentId);
+        List tweetList = tweetMapper.selectList(queryWrapper);
+        int size = tweetList.size();
+        if(size>10) size = 9;
+        JSONArray jsonArray = new JSONArray();
+
+        for(int i=0;i<size; i++){
+            JSONObject object = JSONUtil.parseObj(tweetList.get(i));
+
+        }
     }
 
 
