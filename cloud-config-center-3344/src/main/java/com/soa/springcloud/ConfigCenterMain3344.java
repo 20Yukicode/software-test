d43f05637e7804016f91337b5e176e504e0e35dc
ww67652 <1749113286@qq.com>
2021-12-17 17:02:42 +0800

    分布式配置中心

--- cloud-config-center-3344/src/main/java/com/soa/springcloud/ConfigCenterMain3344.java
+++ cloud-config-center-3344/src/main/java/com/soa/springcloud/ConfigCenterMain3344.java
@@ -12,3 +12,5 @@ public class ConfigCenterMain3344
     }
 }
 
+
+
--- cloud-provider-user8001/src/main/java/com/soa/springcloud/controller/ConfigClientController.java
+++ cloud-provider-user8001/src/main/java/com/soa/springcloud/controller/ConfigClientController.java
@@ -1,4 +1,4 @@
-/*
+
 package com.soa.springcloud.controller;
 
 import lombok.extern.slf4j.Slf4j;
@@ -25,4 +25,4 @@ public class ConfigClientController
 }
 
 
-*/
+
